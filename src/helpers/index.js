4eaa230c9366e29f6688ac9feec90f7b4b964824
Paul <justine.dehonor@gmail.com>
2020-10-03 00:17:08 +0800

    Modified helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,11 +1,23 @@
 export const formatNumber = (nStr) => {
   nStr += '';
-  var x = nStr.split('.'); // [100] [32]
-  var x1 = x[0]; // 100
-  var x2 = x.length > 1 ? '.' + x[1] : '.00'; // > 1 .32
-  var rgx = /(\d+)(\d{3})/;
-  while (rgx.test(x1)) {
-    x1 = x1.replace(rgx, `${1},${2}`);
-  }
-  return x1 + x2;
+  // var x = nStr.split('.'); // [10] [100] [32]
+  // var decimals = x[x.length - 1].length < 0 ? '00' : x[x.length - 1].substring(1,2);
+  // var x1 = x[0]; // 100
+  // var x2 = x.length > 1 ? '.' + x[1] : ''; // > 1 .32
+  // var rgx = /(\d+)(\d{3})/;
+  // while (rgx.test(x1)) {
+  //   x1 = x1.replace(rgx, `${1},${2}`);
+  // }
+  // return x1 + x2;
+  // if (x.length > 1) {
+  //   // let decimals = x[x.length - 1];
+  //   // console.log(decimals);
+  //   // if (decimals) {
+  //   //   decimals =
+  //   //     x[x.length - 1].length < 0 ? '00' : x[x.length - 1].substring(1, 2);
+  //   //   return parseFloat(nStr).toLocaleString('en') + '.' + decimals;
+  //   // }
+  // } else {
+  return parseFloat(nStr).toLocaleString('en') + '.00';
+  // }
 };
